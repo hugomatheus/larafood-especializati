7f8a18873549c604bf7982c049821d3707dc3322
hugomatheus <hg.targino@gmail.com>
2020-09-12 09:51:29 -0300

    Criando injeção de depêndencia no PlanController,  buscando todos os planos e listando os planos na view

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
 
 class PlanController extends Controller
 {
+
+    private $plan;
+
+    public function __construct(Plan $plan)
+    {
+        $this->plan = $plan;
+    }
+
     public function index() {
-        return view('admin.pages.plans.index');
+        $plans = $this->plan->all();
+        return view('admin.pages.plans.index', compact('plans'));
     }
 }
--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -7,6 +7,36 @@
 @stop
 
 @section('content')
-    <p>Listagem dos planos</p>
+    <div class="card">
+        <div class="card-header">
+            #filtros
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th style="width: 50px">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                             <td>
+                                 {{ $plan->name}}
+                             </td>
+                             <td>
+                                 {{ $plan->price}}
+                             </td>
+                             <td>
+                                 <a href="" class="btn btn-warning">ver</a>
+                             </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @stop
 
