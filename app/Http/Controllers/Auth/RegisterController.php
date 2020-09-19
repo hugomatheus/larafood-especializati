3879fb9b0295c23bafd06ec2611f58b35ee9cead
hugomatheus <hg.targino@gmail.com>
2020-09-19 11:57:59 -0300

    Validações de Cadastro do Tenant no Laravel

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,11 +49,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'min:3', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
-            'cnpj' => ['required', 'unique:tenants'],
-            'company' => ['required', 'unique:tenants,name'],
+            'password' => ['required', 'string', 'min:6', 'max:16', 'confirmed'],
+            'cnpj' => ['required', 'numeric', 'digits:14', 'unique:tenants'],
+            'company' => ['required', 'string', 'min:3', 'max:255', 'unique:tenants,name'],
         ]);
     }
 
